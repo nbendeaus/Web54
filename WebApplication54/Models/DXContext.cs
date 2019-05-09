4c6e6f0aa35baf7c18c1874453bdaba933cbe0e1
Nagaraju <nbende@gmail.com>
2019-05-09 22:23:31 +0530

    Add project files.

---
+++ WebApplication54/Models/DXContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication54.Models
+{
+    public class DXContext : DbContext
+    {
+        public DbSet<Dept> Depts { get; set; }
+
+    }
+}
